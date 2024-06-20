00843dce2bf56e8bd4389a6fc141e4d12570f895
Jasmin Cordeiro <playgame0159@gmail.com>
2024-06-20 20:07:30 -0300

    Update Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,ts}",
             "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [require('flowbite/plugin')],
   darkMode:'class'
