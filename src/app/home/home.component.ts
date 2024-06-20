8a5715e6cebbdada84d34d01f46b394195bfdefd
Jasmin Cordeiro <playgame0159@gmail.com>
2024-06-20 19:45:19 -0300

    Updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ThemeToggleComponent } from '../theme-toggle/theme-toggle.component';
+import {ClipboardModule} from '@angular/cdk/clipboard';
+import {Clipboard} from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ThemeToggleComponent],
+  imports: [ThemeToggleComponent,ClipboardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+constructor(private clipboard:Clipboard) { }
 
+  copyLink(link: string){
+    this.clipboard.copy(link);
+  }
 
 }
